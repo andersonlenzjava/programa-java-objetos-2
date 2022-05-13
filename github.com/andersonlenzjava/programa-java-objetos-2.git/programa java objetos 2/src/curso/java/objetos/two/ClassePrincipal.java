903265fb7b76355c83b84577edb11ca309465004
ander <ander@DESKTOP-G3BCR3T>
2022-05-13 19:41:26 -0300

    Trabalhos com listas

--- github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/curso/java/objetos/two/ClassePrincipal.java
+++ github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/curso/java/objetos/two/ClassePrincipal.java
@@ -16,56 +16,33 @@ public class ClassePrincipal {
 		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento");
 		String cpf = JOptionPane.showInputDialog("Qual o numero do CPF");
 
-		/*
-		 * String disciplina1 =
-		 * JOptionPane.showInputDialog("Qual o nome da disciplina 1"); String nota1 =
-		 * JOptionPane.showInputDialog("Qual a nota 1"); String disciplina2 =
-		 * JOptionPane.showInputDialog("Qual o nome da disciplina 2"); String nota2 =
-		 * JOptionPane.showInputDialog("Qual a nota 2"); String disciplina3 =
-		 * JOptionPane.showInputDialog("Qual o nome da disciplina 3"); String nota3 =
-		 * JOptionPane.showInputDialog("Qual a nota 3"); String disciplina4 =
-		 * JOptionPane.showInputDialog("Qual o nome da disciplina 4"); String nota4 =
-		 * JOptionPane.showInputDialog("Qual a nota 4");
-		 */
-
+		
 		Aluno aluno1 = new Aluno();
-
+		
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setNumeroCpf(cpf);
-		aluno1.setDataMatricula("03/09/1992");
-
 		
-		Disciplina disciplina1 = new Disciplina();
-		disciplina1.setDisciplina("Banco de dados");
-		disciplina1.setNota(90);
-
-		aluno1.getDisciplinas().add(disciplina1);
-
-		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("Java");
-		disciplina2.setNota(80);
-
-		aluno1.getDisciplinas().add(disciplina2);
-
-		Disciplina disciplina3 = new Disciplina();
-		disciplina3.setDisciplina("HTML");
-		disciplina3.setNota(77);
-
-		aluno1.getDisciplinas().add(disciplina3);
-
-		Disciplina disciplina4 = new Disciplina();
-		disciplina4.setDisciplina("CSS");
-		disciplina4.setNota(66);
+		//Laços para criar a entrada de dados para várias disciplinas
 		
-		aluno1.getDisciplinas().add(disciplina4);
+		for (int pos = 1; pos <= 4; pos++) {
+			
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + pos + "?");
+			String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + pos + "?");
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+			
+			aluno1.getDisciplinas().add(disciplina); //cada uma vai ter uma posição na memória não precisa ser estatico ele vai criando por si  e dai ela
+		}
 
 //--------------------------------------------------------------------------------------------//		
-		System.out.println("Idade do aluno 1: " + aluno1.getNome());
+		System.out.println("Nome do aluno 1: " + aluno1.getNome());
 		System.out.println("Idade do aluno 1: " + aluno1.getIdade());
-		System.out.println("Idade do aluno 1: " + aluno1.getDataNascimento());
-		System.out.println("Idade do aluno 1: " + aluno1.getNumeroCpf());
+		System.out.println("Data Nascimento do aluno 1: " + aluno1.getDataNascimento());
+		System.out.println("Numero do CPF do aluno 1: " + aluno1.getNumeroCpf());
 		System.out.println("Data da matricula aluno 1: " + aluno1.getDataMatricula());
 		System.out.println("-----------------------------------------------------------------");
 	
