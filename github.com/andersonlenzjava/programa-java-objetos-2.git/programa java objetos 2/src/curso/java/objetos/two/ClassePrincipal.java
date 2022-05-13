6549e7412b8a76a422c497263a4a1671ae77c084
ander <ander@DESKTOP-G3BCR3T>
2022-05-13 18:24:19 -0300

    Commit do projeto.

--- github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/curso/java/objetos/two/ClassePrincipal.java
+++ github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/curso/java/objetos/two/ClassePrincipal.java
@@ -1,9 +1,83 @@
 package curso.java.objetos.two;
 
+import javax.swing.JOptionPane;
+
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
+
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
+
+		/* Criação de um objeto na memória com o endereço dentro da variável aluno1 */
+
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno 1");
+		String idade = JOptionPane.showInputDialog("Qual a idade do aluno");
+		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento");
+		String cpf = JOptionPane.showInputDialog("Qual o numero do CPF");
+
+		/*
+		 * String disciplina1 =
+		 * JOptionPane.showInputDialog("Qual o nome da disciplina 1"); String nota1 =
+		 * JOptionPane.showInputDialog("Qual a nota 1"); String disciplina2 =
+		 * JOptionPane.showInputDialog("Qual o nome da disciplina 2"); String nota2 =
+		 * JOptionPane.showInputDialog("Qual a nota 2"); String disciplina3 =
+		 * JOptionPane.showInputDialog("Qual o nome da disciplina 3"); String nota3 =
+		 * JOptionPane.showInputDialog("Qual a nota 3"); String disciplina4 =
+		 * JOptionPane.showInputDialog("Qual o nome da disciplina 4"); String nota4 =
+		 * JOptionPane.showInputDialog("Qual a nota 4");
+		 */
+
+		Aluno aluno1 = new Aluno();
+
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setNumeroCpf(cpf);
+		aluno1.setDataMatricula("03/09/1992");
+
 		
-	}
+		Disciplina disciplina1 = new Disciplina();
+		disciplina1.setDisciplina("Banco de dados");
+		disciplina1.setNota(90);
+
+		aluno1.getDisciplinas().add(disciplina1);
+
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Java");
+		disciplina2.setNota(80);
+
+		aluno1.getDisciplinas().add(disciplina2);
+
+		Disciplina disciplina3 = new Disciplina();
+		disciplina3.setDisciplina("HTML");
+		disciplina3.setNota(77);
+
+		aluno1.getDisciplinas().add(disciplina3);
+
+		Disciplina disciplina4 = new Disciplina();
+		disciplina4.setDisciplina("CSS");
+		disciplina4.setNota(66);
+		
+		aluno1.getDisciplinas().add(disciplina4);
+
+//--------------------------------------------------------------------------------------------//		
+		System.out.println("Idade do aluno 1: " + aluno1.getNome());
+		System.out.println("Idade do aluno 1: " + aluno1.getIdade());
+		System.out.println("Idade do aluno 1: " + aluno1.getDataNascimento());
+		System.out.println("Idade do aluno 1: " + aluno1.getNumeroCpf());
+		System.out.println("Data da matricula aluno 1: " + aluno1.getDataMatricula());
+		System.out.println("-----------------------------------------------------------------");
 	
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado());
+		
+		System.out.println("Media das notas é  = " + aluno1.getMediaNota());
+
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2() + "\n");
+//--------------------------------------------------------------------------------------------//	
+		System.out.println(aluno1); // imprime tudo os dados do objeto para checagem rapida, la na frente vai ser
+									// feito uma seleção//
+	}
+
 }
--- github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/cursojava/classes/Aluno.java
+++ github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/cursojava/classes/Aluno.java
@@ -1,5 +1,176 @@
 package cursojava.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/* Classe é uma forma que representa o aluno */
 public class Aluno {
+
+	private String nome;
+	private Integer idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+
+	/* forma de instânciar uma lista de disciplina  ATRIBUI A VARIAVEL disciplinas a lista dos atributos de disciplinas*/
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+	this.disciplinas = disciplinas;
+	}
+
+	public List<Disciplina> getDisciplinas() {
+	return disciplinas;
+	}
+
+	public Aluno() {
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	/*
+	 * /* Método get com retorno de um double com o cálculo da média do aluno
+	 */
+	public double getMediaNota() {
+		
+		double somaNotas = 0.0;
+		
+		//laço para fazer uma varedura de todos os atributos do atributo nota
+		
+		for (Disciplina disciplina : disciplinas) {
+			somaNotas += disciplina.getNota();
+		}
+		return somaNotas / disciplinas.size();
+	}
+
+	/* Chamo o método dentro do proprio método */
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/* Normalmente é usado boolean, string fica mais para telas */
+	public String getAlunoAprovado2() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return "Aluno está aprovado";
+		} else {
+			return "Aluno está reprovado";
+		}
+	}
+
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + "]";
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((numeroCpf == null) ? 0 : numeroCpf.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (numeroCpf == null) {
+			if (other.numeroCpf != null)
+				return false;
+		} else if (!numeroCpf.equals(other.numeroCpf))
+			return false;
+		return true;
+	}
 
 }
