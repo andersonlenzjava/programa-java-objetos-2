47a2bb4d7a53caef13d009bdc16672bebfef934e
ander <ander@DESKTOP-G3BCR3T>
2022-05-14 19:49:30 -0300

    Organização com hashMap

--- github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/curso/java/objetos/two/ClassePrincipal5.java
+++ github.com/andersonlenzjava/programa-java-objetos-2.git/programa java objetos 2/src/curso/java/objetos/two/ClassePrincipal5.java
@@ -10,18 +10,15 @@ import cursoconstantes1.java.StatusAluno;
 import cursojava.classes.Aluno;
 import cursojava.classes.Disciplina;
 
-public class ClassePrincipal4 {
+public class ClassePrincipal5 {
 
 	public static void main(String[] args) {
 
 		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
 
 //é uma lista que dentro dela temos uma chave que identifica uma sequecia de valores também 
-
-		List<Aluno> alunosSuperAprovado = new ArrayList<Aluno>();
-		List<Aluno> alunosAprovado = new ArrayList<Aluno>();
-		List<Aluno> alunosRecuperacao = new ArrayList<Aluno>();
-		List<Aluno> alunosreprovados = new ArrayList<Aluno>();
 
 		for (int qtd = 1; qtd <= 5; qtd++) {
 
@@ -51,40 +48,46 @@ public class ClassePrincipal4 {
 
 // laço para varer a lista de alinos e retornar os statos e fazer algo ao encotrar um determinado(usando static)		
 
+		maps.put(StatusAluno.SUPERAPROVADO, new ArrayList<Aluno>());
+		maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
+		maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
+		maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
+		
 		for (Aluno aluno : alunos) {
 			if (aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.SUPERAPROVADO)) {
-				alunosSuperAprovado.add(aluno);
+				maps.get(StatusAluno.SUPERAPROVADO).add(aluno);
 			} else if (aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.APROVADO)) {
-				alunosAprovado.add(aluno);
+				maps.get(StatusAluno.APROVADO).add(aluno);
 			} else if (aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
-				alunosRecuperacao.add(aluno);
+				maps.get(StatusAluno.RECUPERACAO).add(aluno);
 			} else {
-				alunosreprovados.add(aluno);
+				maps.get(StatusAluno.REPROVADO).add(aluno);
 			}
 		}
 
 		System.out.println("-------------------lista de superaprovados--------------------");
-		for (Aluno aluno : alunosSuperAprovado) {
+		for (Aluno aluno : maps.get(StatusAluno.SUPERAPROVADO)) {
 			System.out.println("Resultado = " + aluno.getAlunoAprovado2() + " com média de = " + aluno.getMediaNota()
 					+ " Aluno : " + aluno.getNome());
 		}
 
 		System.out.println("-------------------lista de aprovados-------------------------");
-		for (Aluno aluno : alunosAprovado) {
+		for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
 			System.out.println("Resultado = " + aluno.getAlunoAprovado2() + " com média de = " + aluno.getMediaNota()
 					+ " Aluno : " + aluno.getNome());
 		}
 
 		System.out.println("-------------------lista de em recuperação--------------------");
-		for (Aluno aluno : alunosRecuperacao) {
+		for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
 			System.out.println("Resultado = " + aluno.getAlunoAprovado2() + " com média de = " + aluno.getMediaNota()
 					+ " Aluno : " + aluno.getNome());
 		}
 
 		System.out.println("-------------------lista de reprovados------------------------");
-		for (Aluno aluno : alunosreprovados) {
+		for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
 			System.out.println("Resultado = " + aluno.getAlunoAprovado2() + " com média de = " + aluno.getMediaNota()
 					+ " Aluno : " + aluno.getNome());
 		}
 	}
+
 }
